eb3b06f2dc57ee17f499f2fc88e8d5779deabd65
Radovan Jevric <radovanjevri@gmail.com>
2024-07-02 00:46:30 +0200

    Napravljeni breadcrumbsovi

--- src/data/menu.js
+++ src/data/menu.js
@@ -4,21 +4,24 @@ const appetizers = [
         "name": "spring-rolls",
         "big-portion-price": 200,
         "small-portion-price": 100,
-        "img": "/img/appetizers/spring-rolls.jpg"
+        "img": "/img/appetizers/spring-rolls.jpg",
+        "rates": []
     },
     {
         "id": 2,
         "name": "bao-buns",
         "big-portion-price": 300,
         "small-portion-price": 200,
-        "img": "/img/appetizers/bao-buns.jpg"
+        "img": "/img/appetizers/bao-buns.jpg",
+        "rates": []
     },
     {
         "id": 3,
         "name": "egg-rolls",
         "big-portion-price": 200,
         "small-portion-price": 100,
-        "img": "/img/appetizers/egg-rolls.webp"
+        "img": "/img/appetizers/egg-rolls.webp",
+        "rates": []
     }
 ]
 
