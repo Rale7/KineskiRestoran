bc34dd620f5d0b832e74b7c9647f537881af89bb
Radovan Jevric <radovanjevri@gmail.com>
2024-07-01 14:45:40 +0200

    Namesten bootstrap i carousel

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import i18n from './data/i18n'
 import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+import 'bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 createApp(App).use(router).use(i18n).mount('#app')
